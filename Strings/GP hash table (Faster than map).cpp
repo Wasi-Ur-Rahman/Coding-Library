9b807be80ae229cbb5ac3350c4e27333b59b21fb
GRBs <48942495+Wasi-Ur-Rahman@users.noreply.github.com>
2023-03-04 12:36:53 +0600

    Create GP hash table (Faster than map).cpp

---
+++ Strings/GP hash table (Faster than map).cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+// must include this
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+gp_hash_table<int, int> mp;
