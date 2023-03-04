a778a31cc39d016a6eb37c3c16d090be4f766df3
GRBs <48942495+Wasi-Ur-Rahman@users.noreply.github.com>
2023-03-04 12:31:16 +0600

    Create Segment tree.cpp

---
+++ Data Structures/Segment tree.cpp
@@ -0,0 +1,37 @@
+const int N = 3e5 + 9;
+
+int a[N];
+struct ST {
+  int t[4 * N];
+  static const int inf = 1e9;
+  ST() {
+    memset(t, 0, sizeof t);
+  }
+  void build(int n, int b, int e) {
+    if (b == e) {
+      t[n] = a[b];
+      return;
+    }
+    int mid = (b + e) >> 1, l = n << 1, r = l | 1;
+    build(l, b, mid);
+    build(r, mid + 1, e);
+    t[n] = max(t[l], t[r]); // change this
+  }
+  void upd(int n, int b, int e, int i, int x) {
+    if (b > i || e < i) return;
+    if (b == e && b == i) {
+      t[n] = x; // update
+      return;
+    }
+    int mid = (b + e) >> 1, l = n << 1, r = l | 1;
+    upd(l, b, mid, i, x);
+    upd(r, mid + 1, e, i, x);
+    t[n] = max(t[l], t[r]); // change this
+  }
+  int query(int n, int b, int e, int i, int j) {
+    if (b > j || e < i) return -inf; // return appropriate value
+    if (b >= i && e <= j) return t[n];
+    int mid = (b + e) >> 1, l = n << 1, r = l | 1;
+    return max(query(l, b, mid, i, j), query(r, mid + 1, e, i, j)); // change this
+  }
+}t;
